4ab91b9d452c7243c326584591ce368c98728ed8
Saumya2496 <70735891+Saumya2496@users.noreply.github.com>
2020-09-28 19:17:25 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,10 +1,6 @@
 number1 = input(10) 
 number2 = input(20) 
   
-# Adding two numbers 
-# User might also enter float numbers 
 sum = (number1) + (number2) 
   
-# Display the sum 
-# will print value in float 
 print("The sum of number1 and number2 is sum ") 
